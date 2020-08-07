217da8bfdddd591daac8ff45937d0a8e4bde4c1f
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-08-06 19:25:31 -1000

    class App Component added, state= numInput:0 added, handleInputChange event added.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    
-    </div>
-  );
+class App extends Component {
+  state={
+    numInput:0,
+  };
+
+handleInputChange = event=>{
+  const {name, value} = event.target
+  this.setState({[name]:value})
+}
+  render() {
+    return (
+      <div className="App">
+        <h1>Andrew's International Employee Tracker</h1>
+        <label htmlfor="numInput">
+          # of Emps
+        <input id="numInput" 
+        name="numInput" 
+        value={this.state.numInput} 
+        type="number" 
+        min="0" 
+        onChange={this.handleInputChange}
+        
+        />
+        </label>
+        <button>Submit</button>
+
+        <div className ="empContainer">{this.state.numInput}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
